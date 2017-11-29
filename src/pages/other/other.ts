18e1f224eb032767971c13287e17120521d121d7
Bora Kiriş <bora.kiris@litum.com.tr>
2017-11-29 11:24:51 +0300

    second commit

--- src/pages/other/other.ts
+++ src/pages/other/other.ts
@@ -7,13 +7,26 @@ import { DashboardDataProvider } from './../../providers/dashboard-data/dashboar
 })
 export class OtherDataPage {
 doctorRatio: number;
+doctorRatioTarget: number;
 numOfPackageToFix: number;
 numOfDoctorReels: number;
-stdPacksSentToWarehouse: number;
+doctorStock: number;
 wipPriCoat: number;
 wipCoatSli: number;
+doctorStockTarget: number;
+wipPriCoatTarget: number;
+wipCoatSliTarget: number;
   constructor(public navCtrl: NavController, private dBoardProvider: DashboardDataProvider) {
-
+    this.doctorRatio=this.dBoardProvider.dataMapObj.get("DOCTOR RATIO Yesterday's");
+    this.doctorRatioTarget=this.dBoardProvider.dataMapObj.get("DOCTOR RATIO Target");
+    this.numOfPackageToFix=this.dBoardProvider.dataMapObj.get("NUMBER OF PACKAGES TO FIX");
+    this.numOfDoctorReels=this.dBoardProvider.dataMapObj.get("NUMBER OF DOCTOR REELS ");
+    this.doctorStock=this.dBoardProvider.dataMapObj.get("DOCTOR STOCK");
+    this.doctorStockTarget=this.dBoardProvider.dataMapObj.get("DOCTOR STOCK (Target)");
+    this.wipPriCoat=this.dBoardProvider.dataMapObj.get("WIP PRINTING-COATING");
+    this.wipPriCoatTarget=this.dBoardProvider.dataMapObj.get("WIP PRINTING-COATING (Target) ");
+    this.wipCoatSli=this.dBoardProvider.dataMapObj.get("WIP COATING-SLITTING");
+    this.wipCoatSliTarget=this.dBoardProvider.dataMapObj.get("WIP COATING-SLITTING (Target) ");
   }
 
 }
